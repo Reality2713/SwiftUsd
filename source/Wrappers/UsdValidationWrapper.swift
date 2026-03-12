b489f4d0b766635b4cf62c285bd75313c4899018
Cristian Díaz <cristian@210x7.io>
2026-03-12 23:07:55 +0100

    Expose structured canonical mismatch fields

--- source/Wrappers/UsdValidationWrapper.swift
+++ source/Wrappers/UsdValidationWrapper.swift
@@ -69,6 +69,9 @@ public struct UsdValidationIssueDescriptor: Codable, Sendable, Hashable {
     public let message: String
     public let validatorName: String?
     public let validatorDocumentation: String?
+    public let propertyPath: String?
+    public let expectedType: String?
+    public let actualType: String?
     public let sites: [UsdValidationSiteDescriptor]
     public let fixers: [UsdValidationFixerDescriptor]
 }
